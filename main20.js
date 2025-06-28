e435b9914f826c494d70e6067df92da5fadb091e
bot_kill_site <tantantarou99@gmail.com>
2025-06-28 20:50:24 +0900

    Update main20.js

--- main20.js
+++ main20.js
@@ -2,7 +2,7 @@ const audioPaths = [
   { src: '', volume: 0.2 }, // 1曲目
   { src: '', volume: 0.8 }, // 2曲目
   { src: '', volume: 0.6 }, // 3曲目
-  { src: '/sound-box②/中華風もどき(鬼堂 火猫).mp3', volume: 0.1 }  // 4曲目
+  { src: '中華風もどき(鬼堂 火猫).mp3', volume: 0.1 }  // 4曲目
 ];
 
 const audioElements = audioPaths.map(path => {
@@ -35,7 +35,7 @@ document.addEventListener('DOMContentLoaded', function() {
   // ページ遷移ボタンの効果音処理
   const pageNextButton = document.getElementById('nextButton1');
   const pageBackButton = document.getElementById('backButton2');
-  const pageClickSound = new Audio('/sound-box/PC-Mouse06-1.mp3');
+  const pageClickSound = new Audio('PC-Mouse06-1.mp3');
 
   if (pageNextButton) {
     pageNextButton.addEventListener('click', function() {
@@ -72,9 +72,9 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
   // 効果音の定義とボリューム設定
-  const clickSound1 = new Audio('/sound-box/meka_ge_keyborad01.mp3');
+  const clickSound1 = new Audio('meka_ge_keyborad01.mp3');
   const clickSound2 = new Audio('');
-  const modalClickSound = new Audio('/sound-box/PC-Mouse06-1.mp3');
+  const modalClickSound = new Audio('PC-Mouse06-1.mp3');
 
   const mainClickVolume1 = 0.2;
   const mainClickVolume2 = 0.0;
@@ -352,7 +352,7 @@ function openModal93() {
   const modal93 = document.getElementById("myModal93");
   if (modal93) {
     modal93.style.display = "block";
-    const clickSoundEffectModal52Open = new Audio('/sound-box/PC-Mouse06-1.mp3');
+    const clickSoundEffectModal52Open = new Audio('PC-Mouse06-1.mp3');
     clickSoundEffectModal52Open.play();
   }
 }
@@ -361,7 +361,7 @@ function closeModal93() {
   const modal93 = document.getElementById("myModal93");
   if (modal93) {
     modal93.style.display = "none";
-    const clickSoundEffectModal52Close = new Audio('/sound-box/PC-Mouse06-1.mp3');
+    const clickSoundEffectModal52Close = new Audio('PC-Mouse06-1.mp3');
     clickSoundEffectModal52Close.play();
   }
 }
