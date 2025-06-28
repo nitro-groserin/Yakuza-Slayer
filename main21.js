e20e95f30d1b611a650952f8b974c1fcf6f587b8
bot_kill_site <tantantarou99@gmail.com>
2025-06-28 20:56:41 +0900

    Update main21.js

--- main21.js
+++ main21.js
@@ -1,16 +1,16 @@
 const audioPaths = [
-  { src: '/sound-eat-box/juice-01.mp3', volume: 0.5 }, // 1曲目
-  { src: '/sound-eat-box/mensusuru-01.mp3', volume: 0.3 }, // 1曲目
-  { src: '/sound-eat-box/飲む.mp3', volume: 1.0 }, // 1曲目
-  { src: '/sound-eat-box/咀嚼.mp3', volume: 0.5 }, // 1曲目
-  { src: '/sound-eat-box/咀嚼2.mp3', volume: 0.4 }, // 1曲目
+  { src: 'juice-01.mp3', volume: 0.5 }, // 1曲目
+  { src: 'mensusuru-01.mp3', volume: 0.3 }, // 1曲目
+  { src: '飲む.mp3', volume: 1.0 }, // 1曲目
+  { src: '咀嚼.mp3', volume: 0.5 }, // 1曲目
+  { src: '咀嚼2.mp3', volume: 0.4 }, // 1曲目
   { src: '', volume: 0.2 }, // 1曲目
   { src: '', volume: 0.2 }, // 1曲目
-  { src: '/sound-eat-box/茶碗を置く.mp3', volume: 0.5 }, // 1曲目
+  { src: '茶碗を置く.mp3', volume: 0.5 }, // 1曲目
 
-  { src: '/sound-box/sei_ge_otyawan_gatugatu01.mp3', volume: 0.2 }, // 1曲目
-  { src: '/sound-eat-box/nomu.mp3', volume: 0.3 }, // 2曲目
-  { src: '/sound-eat-box/グラスに水を注ぐ.mp3', volume: 0.5 }, // 3曲目
+  { src: 'sei_ge_otyawan_gatugatu01.mp3', volume: 0.2 }, // 1曲目
+  { src: 'nomu.mp3', volume: 0.3 }, // 2曲目
+  { src: 'グラスに水を注ぐ.mp3', volume: 0.5 }, // 3曲目
   { src: '', volume: 0.1 }  // 4曲目
 ];
 
@@ -44,7 +44,7 @@ document.addEventListener('DOMContentLoaded', function() {
   // ページ遷移ボタンの効果音処理
   const pageNextButton = document.getElementById('nextButton1');
   const pageBackButton = document.getElementById('backButton2');
-  const pageClickSound = new Audio('/sound-box/PC-Mouse06-1.mp3');
+  const pageClickSound = new Audio('PC-Mouse06-1.mp3');
 
   if (pageNextButton) {
     pageNextButton.addEventListener('click', function() {
@@ -81,9 +81,9 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
   // 効果音の定義とボリューム設定
-  const clickSound1 = new Audio('/sound-box/meka_ge_keyborad01.mp3');
+  const clickSound1 = new Audio('meka_ge_keyborad01.mp3');
   const clickSound2 = new Audio('');
-  const modalClickSound = new Audio('/sound-box/PC-Mouse06-1.mp3');
+  const modalClickSound = new Audio('PC-Mouse06-1.mp3');
 
   const mainClickVolume1 = 0.2;
   const mainClickVolume2 = 0.0;
