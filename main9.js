0761936814b9bb2622e5e5d79e42e857eb32ccfa
bot_kill_site <tantantarou99@gmail.com>
2025-06-29 11:06:35 +0900

    Update main9.js

--- main9.js
+++ main9.js
@@ -1,6 +1,6 @@
 const audioPaths = [
-  { src: '/sound-box②/アスファルトの上を走る2.mp3', volume: 1.0 }, // 1曲目
-  { src: '/sound-box②/崩壊都市ー街道ー.mp3', volume: 0.2 }  // 2曲目
+  { src: 'アスファルトの上を走る2.mp3', volume: 1.0 }, // 1曲目
+  { src: '崩壊都市ー街道ー.mp3', volume: 0.2 }  // 2曲目
 ];
 
 const audioElements = audioPaths.map(path => {
@@ -43,7 +43,7 @@ document.addEventListener('DOMContentLoaded', function() {
   // ページ遷移ボタンの効果音処理
   const pageNextButton = document.getElementById('nextButton1');
   const pageBackButton = document.getElementById('backButton2');
-  const pageClickSound = new Audio('/sound-box/PC-Mouse06-1.mp3');
+  const pageClickSound = new Audio('PC-Mouse06-1.mp3');
 
   if (pageNextButton) {
     pageNextButton.addEventListener('click', function() {
@@ -80,9 +80,9 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
   // 効果音の定義とボリューム設定
-  const clickSound1 = new Audio('/sound-box/meka_ge_keyborad01.mp3');
+  const clickSound1 = new Audio('meka_ge_keyborad01.mp3');
   const clickSound2 = new Audio('');
-  const modalClickSound = new Audio('/sound-box/PC-Mouse06-1.mp3');
+  const modalClickSound = new Audio('PC-Mouse06-1.mp3');
 
   const mainClickVolume1 = 0.2;
   const mainClickVolume2 = 0.0;
@@ -424,17 +424,6 @@ if (nextButtonPre98) {
 
 
 
-
-
-
-//「最近、目立ちたがり屋のバカ野郎が多すぎて困るぜ。
-　//指まで指しちゃってよ」
-　
-//「あ？　なんて言ったんだ今？」
-
-
-//「目立ちたがり屋のバカ野郎が多すぎるって言ったんだよ。
-//耳聞こえねえのか？　この野郎」
 
 
 
