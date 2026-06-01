9481d08045f1759f6e72c9427a3e6e7fe42fcf79
bot_kill_site <tantantarou99@gmail.com>
2026-06-01 23:51:48 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,22 @@
-sw.jsを
+// sw.js
+
+// Service Workerがインストールされたときに実行されるイベント
+self.addEventListener('install', (event) => {
+  console.log('Service Worker: インストールされました');
+});
+
+// Service Workerがアクティブになったときに実行されるイベント
+self.addEventListener('activate', (event) => {
+  console.log('Service Worker: アクティブ化されました');
+});
+
+// ページのリクエスト（通信）をインターセプトするイベント
+self.addEventListener('fetch', (event) => {
+  // ここにキャッシュの処理などを書きますが、まずはそのままスルー
+  console.log('Fetchリクエスト:', event.request.url);
+});
+
+
+
+
+
